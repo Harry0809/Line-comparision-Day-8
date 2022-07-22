bc98b2ac726c125e3b94194e1c07f7ced661d238
Harish mahur <hmahur20@gmail.com>
2022-07-22 06:12:25 +0530

    Welcome to line comparision using oops concept

---
+++ LineComparision.java
@@ -0,0 +1,11 @@
+package com.Line.comparision;
+
+public class LineComparision {
+
+	public static void main(String[] args) {
+		// TODO Auto-generated method stub
+		System.out.println("welcome to line comparision using oops concept");
+
+	}
+
+}
