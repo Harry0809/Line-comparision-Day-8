2313ed8148d192b2cac7c7b6ae36a5c94db29724
Harish mahur <hmahur20@gmail.com>
2022-07-22 06:18:57 +0530

    modified: using cartesian formula

--- LineComparision.java
+++ LineComparision.java
@@ -1,11 +1,30 @@
 package com.Line.comparision;
 
+import java.util.Scanner;
+
 public class LineComparision {
 
 	public static void main(String[] args) {
-		// TODO Auto-generated method stub
-		System.out.println("welcome to line comparision using oops concept");
 
+		System.out.println("Welcome to Line Comparison Computation using oops concept ");
+		Scanner sc = new Scanner(System.in);
+		System.out.println("Enter the point for x1 :");
+		double x1 = sc.nextDouble();
+		System.out.println("Enter the point for y1 :");
+		double y1 = sc.nextDouble();
+		System.out.println("Enter the point for x2 :");
+		double x2 = sc.nextDouble();
+		System.out.println("Enter the point for y2 :");
+		double y2 = sc.nextDouble();
+		LineComparision obj = new LineComparision();
+		obj.Calculate_length(x1, y1, x2, y2);
 	}
 
+	public double Calculate_length(double x1, double y1, double x2, double y2) {
+		double len;
+		len = Math.sqrt((Math.pow(x2 - x1, 2)) + Math.pow(y2 - y1, 2));
+		System.out.println("calculated length:" + len);
+		return len;
+	}
 }
+
